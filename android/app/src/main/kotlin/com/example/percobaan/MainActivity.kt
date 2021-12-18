31f2c14398abc1db977de95bc68f29801687edbf
Syahrul <syahrulram12@gmail.com>
2021-12-18 11:17:37 +0700

    Initial commit

---
+++ android/app/src/main/kotlin/com/example/percobaan/MainActivity.kt
@@ -0,0 +1,6 @@
+package com.example.percobaan
+
+import io.flutter.embedding.android.FlutterActivity
+
+class MainActivity: FlutterActivity() {
+}
---
+++ lib/main.dart
@@ -0,0 +1,53 @@
+import 'dart:async';
+
+import 'package:flutter/material.dart';
+import 'Home.dart';
+
+void main() {
+  runApp(MyApp());
+}
+
+class MyApp extends StatelessWidget {
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      debugShowCheckedModeBanner: false,
+      home: SplashScreen(),
+      routes: {'home': (context) => Home()},
+    );
+  }
+}
+
+class SplashScreen extends StatefulWidget {
+  const SplashScreen({Key? key}) : super(key: key);
+
+  @override
+  _SplashScreenState createState() => _SplashScreenState();
+}
+
+class _SplashScreenState extends State<SplashScreen> {
+  void startTimer() {
+    Timer(Duration(seconds: 3), () {
+      Navigator.of(context).pushReplacementNamed('home');
+    });
+  }
+
+  @override
+  void initState() {
+    // TODO: implement initState
+    super.initState();
+    startTimer();
+  }
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      body: Container(
+          decoration: BoxDecoration(
+            image: DecorationImage(
+                image: AssetImage('assets/cover.png'), fit: BoxFit.fill),
+          ),
+          child: null),
+    );
+  }
+}
